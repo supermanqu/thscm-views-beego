329f4ea6040788e4f095bda2ad1bddb0b0866616
yanglingang <lingang.scm@gmail.com>
2015-03-05 12:29:05 +0800

    3-5 1

--- common/mongo/mongo.go
+++ common/mongo/mongo.go
@@ -1,7 +1,7 @@
 package mongo
 
 import (
-	"github.com/astaxie/beego"
+	// "github.com/astaxie/beego"
 	"gopkg.in/mgo.v2"
 )
 
@@ -16,9 +16,13 @@ func Close() {
 	session.Close()
 }
 */
+var TS string
 
 func init() {
-	url := beego.AppConfig.String("mongodb::url")
+
+	url := "mongodb://127.0.0.1:27017"
+
+	// url := beego.AppConfig.String("mongodb::url")
 
 	sess, err := mgo.Dial(url)
 	if err != nil {
@@ -27,4 +31,6 @@ func init() {
 
 	session = sess
 	session.SetMode(mgo.Monotonic, true)
+	TS = "se3w"
+	// TS = beego.AppConfig.String("mongodb::table_space_name")
 }
